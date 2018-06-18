05f16852f982f9f480c2b2bfa970b8232f1e1d5f
Alec Osborne <alec.weston.osborne@gmail.com>
2018-06-17 19:11:25 -0600

    Refactored the entire SearchTableViewController into a View Controller, plus some adjustments to some UI elements

---
+++ PersonalShowReleaseGuide/Views/EpisodeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EpisodeTableViewCell.swift
+//  PersonalShowReleaseGuide
+//
+//  Created by Alec Osborne on 6/14/18.
+//  Copyright © 2018 UnInfamous Games. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
